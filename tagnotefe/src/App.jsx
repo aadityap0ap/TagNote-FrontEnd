fd2de6dbdf87069c861718307b289acaa73d8873
aadityap0ap <adityabharti450ap@gmail.com>
2026-05-15 17:30:20 +0530

    Create reusable Button component with dynamic text props

--- tagnotefe/src/App.jsx
+++ tagnotefe/src/App.jsx
@@ -1,9 +1,9 @@
+import Buttons from "./components/Buttons"
+
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-white">
-      <h1 className="text-small font-bold text-blue-500">
-        React + Tailwind is working 🚀
-      </h1>
+    <div className="">
+      <Buttons text="Delete"/>
     </div>
   )
 }
---
+++ tagnotefe/src/components/Buttons.jsx
@@ -0,0 +1,7 @@
+export default function Buttons({text}){
+    return(
+        <button className="bg-gray-300 text-black px-4 py-2 rounded-lg">
+            {text}
+        </button>
+    )
+}
