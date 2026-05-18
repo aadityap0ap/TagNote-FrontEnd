61c854d7142258100e81e81ad911d496439a5821
aadityap0ap <adityabharti450ap@gmail.com>
2026-05-18 23:28:06 +0530

    Some Bufs Fixed

--- tagnotefe/src/App.jsx
+++ tagnotefe/src/App.jsx
@@ -5,32 +5,32 @@ import Signup from "./Pages/SignUp"
 
 function App() {
   return (
-    // <div className="">
-    //   <Buttons text="Delete"/>
+    <div className="">
+      <Buttons text="Delete"/>
        
-    //   <Card
-    //     title="React Notes"
-    //     content="Today I learned about reusable components and props."
-    //     tags={["react", "frontend", "javascript"]}
-    //   />
+      <Card
+        title="React Notes"
+        content="Today I learned about reusable components and props."
+        tags={["react", "frontend", "javascript"]}
+      />
 
-    //   <Card
-    //     title="DBMS"
-    //     content="Normalization removes redundancy in databases."
-    //     tags={["dbms", "sql", "college"]}
-    //   />
+      <Card
+        title="DBMS"
+        content="Normalization removes redundancy in databases."
+        tags={["dbms", "sql", "college"]}
+      />
 
-    //    <Card
-    //     title="DBMS"
-    //     content="Normalization removes redundancy in databases."
-    //     tags={["dbms", "sql", "college"]}
-    //   />
+       <Card
+        title="DBMS"
+        content="Normalization removes redundancy in databases."
+        tags={["dbms", "sql", "college"]}
+      />
 
 
-    // </div>
-    <div>
-      <Signin/>
     </div>
+    // <div>
+    //   <Signin/>
+    // </div>
   )
 }
 
